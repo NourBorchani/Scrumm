a8d7a65398857b9c0240ea639adcddac764db32b
Soulayma-art <benalayasalma1@gmail.com>
2019-10-25 13:46:43 +0200

    dashborad2

--- examples/scrum.js
+++ examples/scrum.js
@@ -20,16 +20,16 @@ displayTask()
 function displayTask() {
 
     var todoTable = JSON.parse(localStorage.getItem('tasks')) || [];
-    var html =`<h2>Backlog</h2>`;
+    var html =`<h3>Sprint Backlog</h3>`;
     for (let i = 0; i < todoTable.length; i++) {
         
             console.log(todoTable);
 
             html += `<div class="input-group overflow">
            <span>${todoTable[i].task}</span>
-           <a href="#" class="drag">Drag</a>
+        
            <div class="margin-top-10">
-             <button class="button button-backlog">Backlog</button><button class="button button-progress">In Progress</button><button class="button button-progress">In Review</button><button class="button button-done">Done</button><br><button type="button" onclick='deleteTask(${i},${todoTable[i].id},)' class="button button-delete"  >Delete</button>
+             <button class="button button-backlog"><small>Backlog</small></button><button class="button button-progress"><small>In Progress</small></button><button class="button button-progress"><small>In Review</small></button><button class="button button-done"><small>Done</small></button><br><button type="button" onclick='deleteTask(${i},${todoTable[i].id},)' class="button button-delete"><small>Delete</small></button>
            </div>
            </div>`
     }
